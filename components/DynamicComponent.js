2941889864d62c9072270e999361d77b3f79b605
abhay <abhaytxanax@gmail.com>
2023-07-04 10:53:44 +0530

    project-initialized

---
+++ components/DynamicComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DynamicComponent = () => {
+  return (
+    <div>
+      <h1>Hi I am a Dynamic component.</h1>
+    </div>
+  )
+}
+
+export default DynamicComponent
---
+++ pages/test.js
@@ -0,0 +1,10 @@
+
+const test = () => {
+  return (
+    <div>
+      <h1> This is the test page. </h1>
+    </div>
+  )
+}
+
+export default test
