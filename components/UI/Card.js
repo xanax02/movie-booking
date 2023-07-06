920cd718febfafa2a7191058f3bec0f24fbb3fb4
abhay <abhaytxanax@gmail.com>
2023-07-06 19:49:00 +0530

    release-date-filter

--- components/UI/Card.js
+++ components/UI/Card.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
 
-const Card = () => {
-  return (
-    <div>
-      <h1>This is the card Component</h1>
-    </div>
-  )
+import { getImage } from '@/lib/data'
+
+const Card = (props) => {
+
+    const [url, setUrl] = useState();
+
+    useEffect(() => {
+        getImage(props.data.backdrop_path).then(data => setUrl(data))
+    })
+
+    console.log(props.data.release_date);
+
+    return (
+        <Link href={`/booking/upcomming/${props.id}`}>
+            <div className='w-[350px] h-[300px] mr-4 bg-[#252525] rounded-lg hover:scale-105 ease-linear duration-200 flex flex-col relative'>
+                {/* image */}
+                <div className='h-[70%] bg-auto'>
+                    {url && <img className='h-full ' src={url} alt='' />}
+                </div>
+                {/* details */}
+                <div>
+                    <p>{props.data.original_title}</p>
+                </div>
+            </div>
+        </Link>
+    )
 }
 
 export default Card
--- components/row/Row.js
+++ components/row/Row.js
@@ -3,13 +3,24 @@ import Card from '../UI/Card'
 
 const Row = (props) => {
 
-    console.log(props)
-  return (
-    <div>
-        <h1>best</h1>
-      <Card />
-    </div>
-  )
+    const month = new Date().getMonth() +1;
+
+    return (
+        <div className='mx-8 mt-4'>
+            <h1 className='text-2xl font-semibold mb-4'>{props.title}</h1>
+            <div className='w-full flex'>
+                {props.data?.filter((item) => {
+                    const itemMonth = new Date(item.release_date).getMonth() +1;
+                    return (month<=itemMonth)
+                }).map((item,index) => {
+                    return (<Card key={index+1} data={item} />)
+                })}
+                {/* {props.data?.map((item, index) => {
+                    return <Card key={index+1} data={item} />
+                })} */}
+            </div>
+        </div>
+    )
 }
 
 export default Row
