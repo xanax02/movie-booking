8c0d9876ef29662127251301791c41ad8841f442
abhay <abhaytxanax@gmail.com>
2023-07-10 14:50:21 +0530

    updating-bookings

--- pages/api/auth/bookings.js
+++ pages/api/auth/bookings.js
@@ -6,6 +6,17 @@ const handler = async(req, res) => {
 
     const collection = client.db().collection('users');
 
+    const user = collection.findOne({email: req.body.email})
+
+    const update = {
+        $push: {
+            bookings: req.body.booking
+        }
+    }
+
+    const result = await collection.updateOne(user, update);
+    res.status(201).json({message: 'updated successfully'});
+
 }
 
 export default handler; 
